8e827e3d11493b243a2df52d91b4ac669aff441a
SyZeck <93341605+SyZeck@users.noreply.github.com>
2023-06-11 00:17:49 -0600

    Delete Condicionales directory

--- Condicionales/Ejercicio 6/solucion.py
+++
@@ -1,9 +0,0 @@
-Nombre= input("Introduce tu nombre: ")
-Sexo= input("Introduce tu Sexo en formato M=Mujer, H=Hombre: ")
-
-if (Sexo == "M" and Nombre.lower() < 'm') or (Sexo == "H" and Nombre.lower() > 'n'):
-    print ("Tu grupo es el grupo A")
-else:
-    print ("Tu grupo es el grupo B")
-
-	
