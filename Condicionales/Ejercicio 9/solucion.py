c4a39553633409d037f679520a865bf1cd7a4767
SyZeck <93341605+SyZeck@users.noreply.github.com>
2023-06-11 00:20:00 -0600

    Delete Condicionales directory

--- Condicionales/Ejercicio 9/solucion.py
+++
@@ -1,9 +0,0 @@
-edad =int(input("Introduce la edad del cliente: "))
-
-if edad < 4:
-	print("La entrada es gratis. ")
-
-elif 4 <= edad <= 18 :
-	print("La entrada cuesta: 5$.")
-else :
-	print("La entrada cuesta: $10.")
