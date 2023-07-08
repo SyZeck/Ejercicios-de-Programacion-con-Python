2b5eb514e85bf9e76a3d12762cef8a0bb62bba05
SyZeck <93341605+SyZeck@users.noreply.github.com>
2023-07-08 16:13:05 -0600

    Add files via upload

---
+++ Funciones/Ejercicio 6/solucion.py
@@ -0,0 +1,14 @@
+def MediaN(numeros) :
+	resultado = sum(numeros)/len(numeros)
+	return resultado
+
+numeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+med = MediaN(numeros)
+
+print(f"La media de {numeros} es: {med}")
+
+
+
+
+	
