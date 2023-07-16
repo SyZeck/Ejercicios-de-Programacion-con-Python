62ae4b045b1cbaf4f39945c225bba6e3db3608e7
SyZeck <93341605+SyZeck@users.noreply.github.com>
2023-07-16 00:26:34 -0600

    Add files via upload

---
+++ Programación Funcional/Ejercicio 8/solucion.py
@@ -0,0 +1,16 @@
+def Crear(materias, calif) :
+	nuevo = {}
+	
+	for i in range(len(materias)) :
+		materia = materias[i].upper()
+		resultado = calif[i]
+		if resultado >= 70 :
+			nuevo[materia] = resultado
+	return nuevo
+		
+calif = [100, 28, 89, 42,71]
+materias = ['Artes', 'Geografia', 'Matematicas', 'Geografia', 'Español']
+
+imprimir = Crear(materias, calif)
+
+print(imprimir)
